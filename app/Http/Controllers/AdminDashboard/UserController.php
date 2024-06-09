253b1cd32488f7a720a0f839c3241cca9af36850
samer hesham <s.hesham@wrsco.local>
2024-06-09 13:01:30 +0200

    permissions

--- app/Http/Controllers/AdminDashboard/UserController.php
+++ app/Http/Controllers/AdminDashboard/UserController.php
@@ -13,4 +13,18 @@ class UserController extends Controller
         $users = User::all();
         return view('AdminDashboard.users.index', compact('users'));
     }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        User::create($request->all());
+        toastr()->success(__('Admin has been added successfully'));
+        return redirect()->route('users.index');
+    }
 }
--- resources/views/AdminDashboard/users/index.blade.php
+++ resources/views/AdminDashboard/users/index.blade.php
@@ -132,11 +132,31 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Cancel') }}</button>
-                <button type="submit" class="btn btn-success">{{ __('Add Admin') }}</button>
-            </div>
+            <form action="{{ route('users.store') }}" method="POST">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">{{ __('Name') }}</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">{{ __('Email') }}</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">{{ __('Password') }}</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">{{ __('Confirm Password') }}</label>
+                        <input type="password" class="form-control" name="password_confirmation">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary"
+                        data-dismiss="modal">{{ __('Cancel') }}</button>
+                    <button type="submit" class="btn btn-success">{{ __('Add Admin') }}</button>
+                </div>
             </form>
 
         </div>
